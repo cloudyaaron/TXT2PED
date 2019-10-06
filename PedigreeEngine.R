fcee70457789eef2b20084d5c5de38fa01a1fc56
cloudyaaron <cloudyaaron@users.noreply.github.com>
2019-10-06 18:24:20 +1100

    skeleton for paraphase
    for me and wang

--- PedigreeEngine.R
+++ PedigreeEngine.R
@@ -9,7 +9,6 @@ if (length(args) == 0){
   #stop("exit")
   
 }else if (length(args) == 1){
-  print(length(args))
   print(args)
 } else{
   print("$Usage Rsript PedigreeEngine.R sample.txt")
@@ -21,26 +20,49 @@ con <- file(args,"r")
 
 #read line one by one by the provindg file
 line <- readLines(con,n = 1)
+linenum <- 1
+ID <- 1
 while( length(line) != 0 ) {
   
   #if the line was describing a attributes
-  if (grepl("_is",line)== TRUE ){
-    print("attributes")
-    print(line)
+  if (grepl("_is",line) == TRUE ){
     
+    print(paste("attributes:",line))
+    aline <- unlist(strsplit(line," "))
+    if(aline[2] == "gender_is"){
+      
+    }else if(){
+      
+    }
     
   #if the line was describing a relation
-  } else if(grepl("_of",line)== TRUE ){
-    print("relation")
-    print(line)
-  }#can have nore function
-  
-  
-  
-  
+  } else if(grepl("_of",line) == TRUE ){
+    
+    print(paste("relation: ",line))
+    rline <- unlist(strsplit(line," "))
+    if(rline[2] == "father_of"){
+      
+    }else if(){
+      
+    }
+    
+    
+  #alertuser error when line can not be understand
+  } else{
+    showerror <- paste("line ",linenum, " has syntax error")
+    print(showerror)
+    break
+  }
+  #can have nore function
   
   #Set to nextline
-  line=readLines(con,n=1)
+  line<-readLines(con,n=1)
+  linenum <- linenum + 1
 }
 #close connection to the file
 close(con)
+
+#function that output the .ped file
+print_ped <- function(){
+  
+}
